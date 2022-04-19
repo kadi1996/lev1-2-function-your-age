2e5f324754972f4f58ec01ca2962ddc32d828230
Kadi <gueneykadircan@gmail.com>
2022-04-19 16:57:05 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let doubleZahl = document.getElementById('jahr');
+let output = document.getElementById('output');
+// console.log(doubleZahl);
+
+function results() {
+    // console.log(test);
+    output.innerHTML = (2022 - doubleZahl.value);
+}
